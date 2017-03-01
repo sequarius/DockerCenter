a62e15f78d2e5d513032387e1047fb33bf26fe9a
Sequarius <sequarius@gmail.com>
2017-03-01 14:45:19 +0800

    init project

---
+++ node/src/main/java/gov/sequarius/dockercenter/node/DockerCenterNodeApplication.java
@@ -0,0 +1,12 @@
+package gov.sequarius.dockercenter.node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerCenterNodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerCenterNodeApplication.class, args);
+	}
+}
