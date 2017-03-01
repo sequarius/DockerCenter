52bafcdfca6490ce8a9f3d9cca4a531490a761af
Sequarius <sequarius@gmail.com>
2017-03-01 14:36:27 +0800

    init project

---
+++ center/src/main/java/gov/sequarius/dockercenter/center/DockercenterCenterApplication.java
@@ -0,0 +1,12 @@
+package gov.sequarius.dockercenter.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockercenterCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockercenterCenterApplication.class, args);
+	}
+}
---
+++ center/src/test/java/gov/sequarius/dockercenter/center/DockercenterCenterApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.sequarius.dockercenter.center;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DockercenterCenterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
